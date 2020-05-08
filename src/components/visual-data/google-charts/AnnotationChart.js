8f4bff2b71cd2fd6532e6f3ff5398fc2d2dd0a82
rlyhan <richard_han@live.com>
2020-05-09 00:57:53 +1200

    Styling updated

--- src/components/visual-data/google-charts/AnnotationChart.js
+++ src/components/visual-data/google-charts/AnnotationChart.js
@@ -1,84 +1,96 @@
 import React from 'react'
 import Chart from 'react-google-charts'
 
-const AnnotationChart = props => {
-  let chartHeight
-  if (window.screen.width > window.screen.height) chartHeight = 'calc(80vh - 54px)'
-  else chartHeight = '70vh'
+let chartHeight
 
+// If landscape, chart height around 80vh
+// If portrait, chart height 50vh
+if (window.screen.width > window.screen.height) chartHeight = 'calc(80vh - 54px)'
+else chartHeight = '60vh'
+
+const AnnotationChart = props => {
   return (
     <>
     { props.data === [] ?
-      <div>Loading...</div> :
+      <div style={{height: chartHeight, position: 'relative'}}>
+        <div className="loader">
+          <img alt="loading"
+               src={require('../../../images/Rolling-1.2s-100px.gif')} />
+        </div>
+      </div> :
       props.data === null ?
       <div>Could not fetch data. Please try again.</div> :
-      <Chart
-        chartType="AnnotationChart"
-        loader={<div>Loading Chart...</div>}
-        rows={props.data}
-        columns={[
-          {
-            type: "date",
-            label: "Date"
-          },
-          {
-            type: "number",
-            label: props.numberType === "total" ? `TOTAL ${props.caseType.toUpperCase()} COVID-19 CASES TO THIS DAY` : `NEW ${props.caseType.toUpperCase()} COVID-19 CASES RECORDED ON THIS DAY`
-          },
-          {
-            type: "string",
-            label: "Event"
-          },
-          {
-            type: "string",
-            label: "Event description"
-          }
-        ]}
-        chartEvents={[
-          {
-            eventName: 'animationfinish',
-            callback: () => {
-              console.log('Animation Finished')
+      <div className="google-chart">
+        <Chart
+          chartType="AnnotationChart"
+          loader={<div>Loading Chart...</div>}
+          rows={props.data}
+          columns={[
+            {
+              type: "date",
+              label: "Date"
             },
-          },
-        ]}
-        options={{
-          displayAnnotations: true,
-          // displayAnnotationsFilter: true,
-          displayZoomButtons: false,
-          animation: {
-            startup: true,
-            easing: 'in',
-            duration: 5500
-          },
-          width: '100%',
-          height: chartHeight,
-          minHeight: '300px',
-          chart: {
-            backgroundColor: 'black',
-            hAxis: {
-              textStyle: {
-                color: 'white',
-                fontName: 'Quicksand'
-              },
-              gridlines: {
-                color: '#525252'
-              }
+            {
+              type: "number",
+              label: props.numberType === "total" ? `TOTAL ${props.caseType.toUpperCase()} COVID-19 CASES TO THIS DAY` :
+                                                    `NEW ${props.caseType.toUpperCase()} COVID-19 CASES RECORDED ON THIS DAY`
             },
-            vAxis: {
-              gridlines: {
-                color: '#525252'
-              }
+            {
+              type: "string",
+              label: "Event"
             },
-            chartArea: {
-              backgroundColor: 'black',
+            {
+              type: "string",
+              label: "Event description"
             }
-          },
-          annotationsWidth: 25,
-          thickness: '2'
-        }}
-        rootProps={{ 'data-testid': '2' }}
-      /> }
+          ]}
+          chartEvents={[
+            {
+              eventName: 'animationfinish',
+              callback: () => {
+                console.log('Animation Finished')
+              },
+            },
+          ]}
+          options={{
+            displayAnnotations: true,
+            displayAnnotationsFilter: false,
+            displayZoomButtons: false,
+            animation: {
+              startup: true,
+              easing: 'in',
+              duration: 5500
+            },
+            width: '100%',
+            height: chartHeight,
+            minHeight: '300px',
+            chart: {
+              backgroundColor: 'black',
+              hAxis: {
+                textStyle: {
+                  color: 'white',
+                  fontName: 'Quicksand'
+                },
+                gridlines: {
+                  color: '#525252'
+                }
+              },
+              vAxis: {
+                gridlines: {
+                  color: '#525252'
+                }
+              },
+              chartArea: {
+                backgroundColor: 'black',
+              }
+            },
+            annotationsWidth: 25,
+            thickness: '2'
+          }}
+          rootProps={{ 'data-testid': '2' }}
+        />
+        <div className="annotation-toggle"/>
+      </div> }
     </>
   )
 }
